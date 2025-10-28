4ad40b70f180118269413b177992b0ce94f3edc2
stpercell <stpercell@aggies.ncat.edu>
2025-10-28 19:40:53 -0400

    Update project1_starter.py
    First test case

--- project1_starter.py
+++ project1_starter.py
@@ -1,13 +1,43 @@
 """
 COMP 163 - Project 1: Character Creator & Saving/Loading
-Name: [Your Name Here]
-Date: [Date]
+Name: [Shikel Percell]
+Date: [10/27/25]
 
 AI Usage: [Document any AI assistance used]
 Example: AI helped with file I/O error handling logic in save_character function
 """
-
+#==================================================================my code part 1==================================================================================
+def calculate_stats(character_class):
+    character_class = character_class.lower()
+#Took inspo from my favorite game (metaphor refantazio)
+    if character_class == "mage":#spells
+        return {"strength": 5, "magic": 15,"agility": 3, "health": 80, "gold": 100}
+    elif character_class == "warrior":#basic all rounder
+        return {"strength": 12, "magic": 3,"agility": 8, "health": 120, "gold": 100}
+    elif character_class == "thief":#Speed
+        return {"strength": 8, "magic": 5,"agility": 15, "health": 100, "gold": 100}
+    elif character_class == "brawler":#as the name entails
+        return {"strength": 18, "magic": 1,"agility": 5, "health": 150, "gold": 100}
+    elif character_class == "priest":#healer class
+        return {"strength": 3, "magic": 12,"agility": 8, "health": 50, "gold": 100}
+    elif character_class == "saiyans":#secret dbz class
+        return {"strength": 9000, "magic": 20000,"agility": 9000, "health": 10000, "gold": 10000}#magic is ki and gold is zeni
+#all stat calcs with diff classes
 def create_character(name, character_class):
+    stats = calculate_stats(character_class)
+    character = {
+        "name": name,
+        "class": character_class,
+        "level": 1,
+        "strength": stats["strength"],
+        "magic": stats["magic"],
+        "agility": stats["agility"],
+        "health": stats["health"],
+        "gold": stats["gold"]
+    }
+    return character
+character = create_character("Aria", "saiyans")
+print(character)
     """
     Creates a new character dictionary with calculated stats
     Returns: dictionary with keys: name, class, level, strength, magic, health, gold
@@ -19,6 +49,15 @@ def create_character(name, character_class):
     # TODO: Implement this function
     # Remember to use calculate_stats() function for stat calculation
     pass
+
+
+
+
+
+
+
+#==================================================================my code part 2==================================================================================
+
 
 def calculate_stats(character_class, level):
     """
